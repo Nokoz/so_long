cd5fdb4642a5f442780e44dda38bf9977550882a
Guillaume Vardakis <gvardaki@mi-r8-p3.s19.be>
2023-09-08 11:04:38 +0200

    ->img loader

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: gvardaki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 11:06:55 by gvardaki          #+#    #+#             */
-/*   Updated: 2023/09/06 15:48:04 by gvardaki         ###   ########.fr       */
+/*   Updated: 2023/09/08 11:02:36 by gvardaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ int	main(int ac, char** av)
 
 	game = malloc(sizeof(t_game));
 	ft_parse_map(av[1], game);
-	ft_init_mlx(game);
-//	ft_load_image(game);
+	ft_init_win(game);
+	//ft_load_image(game);
 	//ft_check_map
-	//ft_init_win
 	//ft_game
+	mlx_loop(game->mlx_ptr);
 	ft_free_full(game);
 	return (0);
 }
@@ -47,12 +47,32 @@ void	ft_free_map(t_game *game)
 	free(game->map);
 }
 
-void ft_init_mlx(t_game *game)
+void ft_init_win(t_game *game)
 {
 	game->mlx_ptr = mlx_init();
+	game->map_x = ft_strlen(game->map[0]);
+	ft_printf("ici\n");
+	game->win_ptr = mlx_new_window(game->mlx_ptr, game->map_x * W, game->map_y * H,
+			"so_long");
 }
 
 void	ft_load_image(t_game *game)
 {
-	(void)game;	
+	game->player_left = ft_sprite(game, "img/blue_left.xpm");
+	game->player_right = ft_sprite(game, "img/blue_right.xpm");
+	game->player_down = ft_sprite(game, "img/blue_down.xpm");
+	game->player_up = ft_sprite(game, "img/blue_up.xpm");
+	game->floor = ft_sprite(game, "img/white.xpm");
+	game->wall = ft_sprite(game, "img/grey.xpm");
+	game->collectible = ft_sprite(game, "img/yellow.xpm");
+	game->exit = ft_sprite(game, "img/red.xpm");
+	game->exit_open = ft_sprite(game, "img/green.xpm");
+}
+
+t_sprite	ft_sprite(t_game *game, char *path)
+{
+	t_sprite	sprite;
+
+	sprite.ptr = mlx_xpm_file_to_image(game->mlx_ptr, path, &sprite.x, &sprite.y);
+	return (sprite);
 }
